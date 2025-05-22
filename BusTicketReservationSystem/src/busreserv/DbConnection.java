b121e913eb3e9f23c4a7a341ffb357d277320b7a
Care Pix <Care Pix@DESKTOP-4FAN8U2>
2025-05-21 13:36:40 -1200

    Initial Commit

---
+++ BusTicketReservationSystem/src/busreserv/DbConnection.java
@@ -0,0 +1,13 @@
+package busreserv;
+import java.sql.*;
+public class DbConnection {
+	private static final String url="jdbc:mysql://localhost:3306/busreservation";
+	private static final String userName="root";
+	private static final String passWord="Savitha2004@";
+	
+	
+	public static Connection getConnection() throws SQLException{
+		return  DriverManager.getConnection(url,userName,passWord);
+	}
+
+}
---
+++ BusTicketReservationSystem/src/module-info.java
@@ -0,0 +1,3 @@
+module BusTicketReservationSystem {
+	requires java.sql;
+}
